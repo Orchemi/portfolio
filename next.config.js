23e21461bdeb1de02e655505dc3d3239d7e8b03e
박승훈 <psh1126@midasin.com>
2023-11-01 02:43:25 +0900

    Feat: svgr 설치 및 svg 사용 준비

--- next.config.js
+++ next.config.js
@@ -19,4 +19,11 @@ module.exports = {
       },
     ];
   },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
 };
