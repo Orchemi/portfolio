aa7e3e9949754b776b4c4d6b965ae09272e872ca
박승훈 <psh1126@midasin.com>
2023-11-06 23:10:35 +0900

    Feat: 파네시아 navbar 메뉴 1단 구현

--- src/components/panmnesia/navbar/PanmnesiaNavbar.tsx
+++ src/components/panmnesia/navbar/PanmnesiaNavbar.tsx
@@ -1,6 +1,8 @@
+import Icon from '@/common/icon/Icon';
 import style from './PanmnesiaNavbar.module.scss';
 import classNames from 'classnames/bind';
 import Image from 'next/image';
+import { COLORS } from '@/constants/colors';
 
 const cx = classNames.bind(style);
 
@@ -8,6 +10,18 @@ export default function PanmnesiaNavbar() {
   return (
     <nav className={cx('container')}>
       <Image src={'/assets/imgs/panmnesia/panmnesia_logo.png'} alt={'panmnesia-logo'} width={166} height={30} />
+      <ul className={cx('menu-section')}>
+        <li className={cx('menu-item')}>
+          Home
+          <Icon name={'arrow_drop_down_filled'} color={COLORS.white} size={30} />
+        </li>
+        <li className={cx('menu-item')}>
+          Newsroom
+          <Icon name={'arrow_drop_down_filled'} color={COLORS.white} size={30} />
+        </li>
+        <li className={cx('menu-item')}>Careers</li>
+      </ul>
+      <Icon className={cx('not-pc-hamburger')} name={'move_line'} color={COLORS.white} size={32} />
     </nav>
   );
 }
