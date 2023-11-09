259f247077a4d64c21f924c46e07b2a17c9b1716
박승훈 <psh1126@midasin.com>
2023-11-10 00:28:34 +0900

    Feat: 파네시아 홈 header 2차 구현

--- src/components/panmnesia/home/PanmnesiaHome.tsx
+++ src/components/panmnesia/home/PanmnesiaHome.tsx
@@ -2,9 +2,14 @@
 
 import PanmnesiaHomeHeader from '@/components/panmnesia/home/PanmnesiaHomeHeader';
 
+import classNames from 'classnames/bind';
+import style from './PanmnesiaHome.module.scss';
+
+const cx = classNames.bind(style);
+
 export default function PanmnesiaHome() {
   return (
-    <div>
+    <div className={cx('home')}>
       <PanmnesiaHomeHeader />
     </div>
   );
