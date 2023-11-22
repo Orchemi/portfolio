c425b4e97262c1ba8eb8daf7d260244c55fe8249
박승훈 <psh1126@midasin.com>
2023-11-22 23:41:59 +0900

    Feat: Midas Menu Edit 추가

---
+++ src/app/project/daily/midas-menu/edit/page.tsx
@@ -0,0 +1,5 @@
+import MidasMenuEdit from '@/components/project/daily/midas-menu/edit/MidasMenuEdit';
+
+export default function MidasMenuEditPage() {
+  return <MidasMenuEdit />;
+}
---
+++ src/components/project/daily/midas-menu/edit/MidasMenuEdit.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+import classNames from 'classnames/bind';
+import style from './MidasMenuEdit.module.scss';
+
+const cx = classNames.bind(style);
+
+export default function MidasMenuEdit() {
+  return <div className={cx('home')}>Midas Menu Edit</div>;
+}
