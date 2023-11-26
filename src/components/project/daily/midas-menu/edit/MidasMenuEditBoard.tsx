93ff6caf9b84e00a835c59801205de78ae600cb2
psh1126 <psh1126@midasin.com>
2023-11-26 12:22:48 +0900

    Feat: midas menu edit board row 1차 구성

--- src/components/project/daily/midas-menu/edit/MidasMenuEditBoard.tsx
+++ src/components/project/daily/midas-menu/edit/MidasMenuEditBoard.tsx
@@ -6,5 +6,28 @@ import style from './MidasMenuEditBoard.module.scss';
 const cx = classNames.bind(style);
 
 export default function MidasMenuEditBoard() {
-  return <div className={cx('menu-edit-board')}>Menu Edit Board</div>;
+  return (
+    <div className={cx('board-container')}>
+      <BoardRow date={'날짜'} menus={['조식', '중식', '석식']} />
+      <BoardRow date={'11/20\n(월)'} menus={['조식', '중식', '석식']} />
+    </div>
+  );
+}
+
+interface IBoardRowProps {
+  date: string;
+  menus: string[];
+}
+
+function BoardRow({ date, menus }: IBoardRowProps) {
+  return (
+    <div className={cx('board-row')}>
+      <div className={cx('date')}>{date}</div>
+      {menus.map((menu, index) => (
+        <div className={cx('menu')} key={index}>
+          {menu}
+        </div>
+      ))}
+    </div>
+  );
 }
