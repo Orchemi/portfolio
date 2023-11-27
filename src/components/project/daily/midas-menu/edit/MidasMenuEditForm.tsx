8d4467c3f3092e29ad969bed2084462f0de04bbc
psh1126 <psh1126@midasin.com>
2023-11-27 23:48:29 +0900

    Style: Edit Form Style 개선

--- src/components/project/daily/midas-menu/edit/MidasMenuEditForm.tsx
+++ src/components/project/daily/midas-menu/edit/MidasMenuEditForm.tsx
@@ -11,9 +11,14 @@ export default function MidasMenuEditForm() {
   const { dateRegister, timeRegister, menuRegister, onSubmit, watch } = useMidasMenuEditForm();
   return (
     <form className={cx('menu-edit-form')} onSubmit={onSubmit}>
-      <input type={'date'} {...dateRegister} />
-      <input type={'text'} value={MENU_TIME_DEFINITION[watch('time')]} {...timeRegister} />
-      <textarea className={cx('input-area')} {...menuRegister}></textarea>
+      <input className={cx('input', 'date')} type={'date'} {...dateRegister} />
+      <input
+        className={cx('input', 'time')}
+        type={'text'}
+        value={MENU_TIME_DEFINITION[watch('time')]}
+        {...timeRegister}
+      />
+      <textarea className={cx('input', 'menu')} {...menuRegister}></textarea>
       <button className={cx('submit-btn')} type={'submit'}>
         제출
       </button>
