b21a8fe7bed67847de30149a478505be7abdc8b2
psh1126 <psh1126@midasin.com>
2023-11-28 16:36:43 +0900

    Feat: 현재 날짜가 있는 월화수목금 확인 및 board에 적용

--- src/components/project/daily/midas-menu/edit/MidasMenuEditBoard.tsx
+++ src/components/project/daily/midas-menu/edit/MidasMenuEditBoard.tsx
@@ -2,14 +2,27 @@
 
 import classNames from 'classnames/bind';
 import style from './MidasMenuEditBoard.module.scss';
+import dayjs from 'dayjs';
+import { useMemo } from 'react';
 
 const cx = classNames.bind(style);
 
 export default function MidasMenuEditBoard() {
+  const thisWeek = useMemo(() => {
+    const days = ['일', '월', '화', '수', '목', '금', '토'];
+    const todayDay = dayjs().get('day');
+    return [1, 2, 3, 4, 5].map((v) => {
+      const day = dayjs().add(v - todayDay, 'day');
+      return `${day.format('MM/DD')}\n(${days[day.get('day')]})`;
+    });
+  }, []);
+
   return (
     <div className={cx('board-container')}>
       <BoardRow date={'날짜'} menus={['조식', '중식', '석식']} />
-      <BoardRow date={'11/20\n(월)'} menus={['조식', '중식', '석식']} />
+      {thisWeek.map((date) => {
+        return <BoardRow key={date} date={date} menus={['조식', '중식', '석식']} />;
+      })}
     </div>
   );
 }
