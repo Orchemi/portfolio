53a4ae1da128d4b31b97e95c85f14cc44a322205
psh1126 <psh1126@midasin.com>
2023-12-30 22:35:28 +0900

    Feat: 전체 회원 조회 api
    https://www.youtube.com/watch?v=35lLym8tlQ8

---
+++ src/app/api/user/route.ts
@@ -0,0 +1,29 @@
+import { connectToDatabase, disconnectToDatabase } from '@/helpers/server-helpers';
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+  try {
+    await connectToDatabase();
+    const users = await prisma?.user.findMany();
+    return NextResponse.json(
+      {
+        users,
+      },
+      {
+        status: 200,
+      },
+    );
+  } catch (error) {
+    console.error(error);
+    NextResponse.json(
+      {
+        error: 'server error',
+      },
+      {
+        status: 500,
+      },
+    );
+  } finally {
+    await disconnectToDatabase();
+  }
+}
