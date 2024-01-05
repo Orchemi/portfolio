ff2f48899bde31f93d3250ce0ed1dfb012f0896b
psh1126 <psh1126@midasin.com>
2024-01-05 18:56:50 +0900

    Feat: Nav에 auth 추가

--- src/components/common/Nav.tsx
+++ src/components/common/Nav.tsx
@@ -1,20 +1,35 @@
+'use client';
+
 import classNames from 'classnames/bind';
 import styles from './Nav.module.scss';
 import Link from 'next/link';
+import { signOut, useSession } from 'next-auth/react';
 
 const cx = classNames.bind(styles);
 
 export default function Nav() {
+  const { data, status } = useSession();
+
   return (
     <header className={cx('header')}>
       <nav className={cx('nav')}>
         <div>My Site</div>
         <div className={cx('menu-area')}>
-          <Link href={'/'}>Home</Link>
+          {/* <Link href={'/'}>Home</Link>
           <Link href={'/createUser'}>Create User</Link>
           <Link href={'/clientMember'}>Client Member</Link>
           <Link href={'/member'}>Member</Link>
-          <Link href={'/public'}>Public</Link>
+          <Link href={'/public'}>Public</Link> */}
+          {status === 'unauthenticated' ? (
+            <div className={cx('auth-wrapper')}>
+              <Link href={'/api/auth/signin'}>Login</Link>
+            </div>
+          ) : (
+            <div className={cx('auth-wrapper')}>
+              <span>{data?.user?.name}</span>
+              <button onClick={async () => await signOut()}>LOGOUT</button>
+            </div>
+          )}
         </div>
       </nav>
     </header>
