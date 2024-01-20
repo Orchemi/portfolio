af72bb92d76efbf00535a6fc010caa1f8e0de822
psh1126 <psh1126@midasin.com>
2024-01-20 14:42:47 +0900

    Refact: api-query rq, rs interface 분리

--- src/queries/auth.ts
+++ src/queries/auth.ts
@@ -1,23 +1,12 @@
 // import { postSignUp } from '@/apis/auth';
 import { postSignUp } from '@/apis/auth';
+import { IPostSignUpRequest, IPostSignUpResponse } from '@/interfaces/auth/IPostSignUp';
 import { UseMutationResult, useMutation } from '@tanstack/react-query';
 import { AxiosError } from 'axios';
 export const AuthQueryKey = {
   all: ['auth'] as const,
   signUp: () => [...AuthQueryKey.all, 'signUp'] as const,
 };
-
-export interface IPostSignUpRequest {
-  name: string;
-  email: string;
-  password: string;
-}
-
-export interface IPostSignUpResponse {
-  name: string;
-  email: string;
-  hashedPassword: string;
-}
 
 export function useMutationPostSignUp(): UseMutationResult<IPostSignUpResponse, AxiosError, IPostSignUpRequest> {
   return useMutation({
